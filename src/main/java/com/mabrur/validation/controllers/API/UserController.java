ec98a21a22471474e3dfe40baad1db592a77fba0
mabrursatori <mabrursatori@gmail.com>
2022-08-20 10:18:19 +0700

    update error

--- src/main/java/com/mabrur/validation/controllers/API/UserController.java
+++ src/main/java/com/mabrur/validation/controllers/API/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,7 +39,7 @@ public class UserController {
     }
 
     @PostMapping
-    private User save(@Valid UserForm user){
+    private User save(@Valid @RequestBody UserForm user){
         User newUser = modalMapper.map(user, User.class);
 
         return userService.save(newUser);
--- src/main/java/com/mabrur/validation/data/UserForm.java
+++ src/main/java/com/mabrur/validation/data/UserForm.java
@@ -3,9 +3,13 @@ package com.mabrur.validation.data;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserForm {
 
     private Long id;
